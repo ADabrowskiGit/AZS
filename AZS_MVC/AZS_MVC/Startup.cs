484c358fb20ba36e26dce18105560917ec58125f
Totalit <Totalit@TOTALIT-PC>
2017-12-10 16:07:11 +0100

    Initial commit

---
+++ AZS_MVC/AZS_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AZS_MVC.Startup))]
+namespace AZS_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
